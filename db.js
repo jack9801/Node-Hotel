5753c5b4742aada8514d56aa3289ebff90f76ac7
jack9801 <shishupaljnvr@gmail.com>
2025-05-11 23:40:18 +0530

    mistake in MONGODB_URL

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 //const mongoURL='mongodb://localhost:27017/hotels';
-const mongoURL=process.env.MONGO_URL ;// Use the environment variable for MongoDB connection string
+const mongoURL=process.env.MONGODB_URL ;
 mongoose.connect(mongoURL,
      { useNewUrlParser: true, useUnifiedTopology: true });
+
 const db = mongoose.connection;
 db.on('connected', () => {
     console.log('MongoDB connected successfully')});
